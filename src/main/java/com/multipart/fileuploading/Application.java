e3c7cb006e191b5e967458847e524f51f2610aea
Dinesh Sawake <shraddha.rikhe@perennialsys.com>
2021-07-29 14:48:53 +0530

    multipart file uploding implementation

---
+++ src/main/java/com/multipart/fileuploading/Application.java
@@ -0,0 +1,29 @@
+package com.multipart.fileuploading;
+
+import com.multipart.fileuploading.service.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+
+    @Resource
+    FilesStorageService storageService;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Override
+    public void run(String... arg) throws Exception {
+        storageService.deleteAll();
+        storageService.init();
+    }
+}
+
